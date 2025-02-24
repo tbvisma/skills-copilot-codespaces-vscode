a68110b5970b123c1fe7f18dfabe85aa73b76f18
tbvisma <144991692+tbvisma@users.noreply.github.com>
2025-02-24 09:34:03 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  return {
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Node', 'MongoDB'],
+    getSkills: function() {
+      return this.skills;
+    },
+  };
+}
